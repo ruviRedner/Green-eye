9c26c97081773bb5585befeb245c106c0ce52e6f
ruviRedner <r.y.r7966@gmail.com>
2024-10-07 15:15:16 +0300

    make spaces between objects

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,21 +4,26 @@ const userSechma = new mongoose.Schema({
     user_name :{
         type: String,
         required: [true,"user name is requierd"],
-        minlength:[5,"way too short name"]},
+        minlength:[5,"way too short name"]
+    },
     password:{
         type: String,
-        required: [true,"password must be provided"]},
+        required: [true,"password must be provided"]
+    },
     roll:{
          type:String,
           enum:["soldier","commander"],
-          required: [true,"please provide a roll"]},
+          required: [true,"please provide a roll"]
+        },
     area:{
         type:String,
         enum:["center","south","east","west","north"],
-        required: [true,"please provide an area"]},
+        required: [true,"please provide an area"]
+    },
     units:{
         type:[Number],
-        required:[true,"please specify a unit"]},
+        required:[true,"please specify a unit"]
+    },
 });
 
 const UserModel = mongoose.model("user", userSechma)
