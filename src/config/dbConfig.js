96ac195b6fbb5e62a11a0ea806c72317a0496bdf
ruviRedner <r.y.r7966@gmail.com>
2024-10-07 15:24:59 +0300

    connction to mongo done

---
+++ src/config/dbConfig.js
@@ -0,0 +1,14 @@
+//connction to mongoDB
+const mongoose = require('mongoose');
+
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect("mongodb://localhost:27017/green_eye");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = {
+    connectToMongo
+};
