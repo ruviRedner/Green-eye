faaded9c4bdefce5d5dac5095838bf05e5eb51f0
ruviRedner <r.y.r7966@gmail.com>
2024-10-07 14:47:13 +0300

    user model done

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,13 +1,26 @@
 const mongoose = require('mongoose')
 
 const userSechma = new mongoose.Schema({
-    user_name : {type: String,required: [true,"user name is requierd"],minlength:[5,"way too short name"]},
-    password: {type: String,required: [true,"password must be provided"]},
-    roll:{ type:String, enum:["soldier","commander"],required: [true,"please provide a roll"]},
-    
-
-})
-
-
+    user_name :{
+        type: String,
+        required: [true,"user name is requierd"],
+        minlength:[5,"way too short name"]},
+    password:{
+        type: String,
+        required: [true,"password must be provided"]},
+    roll:{
+         type:String,
+          enum:["soldier","commander"],
+          required: [true,"please provide a roll"]},
+    area:{
+        type:String,
+        enum:["center","south","east","west","north"],
+        required: [true,"please provide an area"]},
+    units:{
+        type:[Number],
+        required:[true,"please specify a unit"]},
+});
 
 const UserModel = mongoose.model("user", userSechma)
+
+module.exports = UserModel;
