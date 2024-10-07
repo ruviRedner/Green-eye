e521eae14e720037759cebf60b2c48f51dd3cfd5
ruviRedner <r.y.r7966@gmail.com>
2024-10-07 17:15:47 +0300

    call login in the controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,11 @@
+const {login:loginFromService} = require("../services/authService")
 const login = async(req,res)=>{
     try {
-      
+      const token = await loginFromService(req.body)
+      res.cookie("token", token)
+      res.status(200).json({message: `${req.body.user_name} so good to see you`})
     } catch (error) {
-      
+      console.log(error.message);
     }}
   
   
