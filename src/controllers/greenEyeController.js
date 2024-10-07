6c10a9973378b9cd4bb2ea21c37d83afb80667e4
ruviRedner <r.y.r7966@gmail.com>
2024-10-07 11:28:58 +0300

    empty controllers createt

---
+++ src/controllers/greenEyeController.js
@@ -0,0 +1,43 @@
+const createPoll = async(req,res)=>{
+    try {
+      
+    } catch (error) {
+      
+    }}
+  
+  
+const getNotification = async(req,res)=>{
+      try {
+        
+      } catch (error) {
+        
+      }}
+  
+  
+const getPollById = async(req,res)=>{
+      try {
+            
+      } catch (error) {
+            
+      }}
+const reply = async(req,res)=>{
+        try {
+              
+        } catch (error) {
+              
+        }}
+const deletePoll = async(req,res)=>{
+    try {
+                  
+    } catch (error) {
+                  
+ }}
+  
+  
+  module.exports = {
+      createPoll,
+      getNotification,
+      getPollById,
+      reply,
+      deletePoll
+      }
---
+++ src/controllers/userController.js
@@ -0,0 +1,29 @@
+const register = async(req,res)=>{
+  try {
+    
+  } catch (error) {
+    
+  }}
+
+
+const getProfile = async(req,res)=>{
+    try {
+      
+    } catch (error) {
+      
+    }}
+
+
+const setSetting = async(req,res)=>{
+    try {
+          
+    } catch (error) {
+          
+    }}
+
+
+module.exports = {
+    register,
+    getProfile,
+    setSetting
+    }
