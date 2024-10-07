25514fc63bbe2cba23c0a3efd0e237b8cf62e0d6
ruviRedner <r.y.r7966@gmail.com>
2024-10-07 14:27:58 +0300

    change the type from "string" to String of mongoose

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 
 const userSechma = new mongoose.Schema({
-    user_name : {type: 'string',required: [true,"user name is requierd"],minlength:[5,"way too short name"]},
-    password: {type: 'string',required: [true,"password must be provided"]},
-    roll:{ type:String, enum:["soldier","commander"],required: [true,"please provide a roll"]}
+    user_name : {type: String,required: [true,"user name is requierd"],minlength:[5,"way too short name"]},
+    password: {type: String,required: [true,"password must be provided"]},
+    roll:{ type:String, enum:["soldier","commander"],required: [true,"please provide a roll"]},
+    
 
 })
 
