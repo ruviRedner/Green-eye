bc4f506febbbecfa38165d1e6be075029775df2a
ruviRedner <r.y.r7966@gmail.com>
2024-10-08 12:26:41 +0300

    connecting swagger to the app as middlware

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const exp = require('express')
 const dotenv = require('dotenv')
 const { connectToMongo } = require('./config/dbConfig')
 const cookieParser = require('cookie-parser')
+const { specs, swaggerUi } = require('./config/swagger');
 
 const app = exp();
 dotenv.config();
@@ -11,6 +12,7 @@ const port = process.env.PORT || 3000
 
 app.use(exp.json())
 app.use(cookieParser())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use("/auth" ,require("./router/authRouter"))
 app.use("/user" ,require("./router/userRouter"))
